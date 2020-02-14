000199c3cc34762173e697ec8e3e4c30be759aac
zhi.zhong <zhong.zhi@163.com>
2020-02-14 18:09:09 +0800

    chore: base 使用 babel-eslint parser

--- packages/eslint-config-qunar-node/index.js
+++ packages/eslint-config-qunar-node/index.js
@@ -1,14 +1,10 @@
 module.exports = {
   extends: ['eslint-config-qunar-base'],
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       // 允许使用旧的装饰器语法
       // https://github.com/babel/babel-eslint/issues/662
       legacyDecorators: true
     }
-  },
-  env: {
-    node: true
   }
 };
--- packages/eslint-config-qunar-react/index.js
+++ packages/eslint-config-qunar-react/index.js
@@ -2,17 +2,12 @@ const rules = require('./rules');
 
 module.exports = {
   extends: ['eslint-config-airbnb', 'eslint-config-qunar-base'],
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       // 允许使用旧的装饰器语法
       // https://github.com/babel/babel-eslint/issues/662
       legacyDecorators: true
     }
-  },
-  env: {
-    es6: true,
-    browser: true
   },
   plugins: ['react'],
   rules
