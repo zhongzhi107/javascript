759142c43e7101422feb0c143df25a2c16157e5f
zhi.zhong <zhong.zhi@163.com>
2020-04-28 14:38:52 +0800

    feat: eslint-config-qunar-typescript-node 的 env 加上 es2017 和 es2020

--- packages/eslint-config-qunar-typescript-node/index.js
+++ packages/eslint-config-qunar-typescript-node/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['eslint-config-qunar-typescript-base'],
   env: {
-    node: true
+    node: true,
+    es2017: true,
+    // 2020 包含 BigInit ，这个在 node 工程里可能会用到
+    es2020: true
+  
   }
 };
