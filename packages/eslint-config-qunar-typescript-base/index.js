97f5d94ac87e39da6a070b55be00175ae531d43c
zhi.zhong <zhong.zhi@163.com>
2020-05-20 19:44:27 +0800

    feat: 修改 camelcase 规则

--- packages/eslint-config-qunar-typescript-base/index.js
+++ packages/eslint-config-qunar-typescript-base/index.js
@@ -14,6 +14,13 @@ module.exports = {
   rules: {
     // 使用 ts 类型时会有问题，先禁用
     'no-unused-vars': 'off',
+
+    // 以下 ts 中类型声明会报错
+    // export type CqpMap = {
+    //   h_home_rn?: Maybe<Scalars['Int']> // h_home_rn 为后端返回的字段
+    // }
+    camelcase: ['error', {ignoreDestructuring: 'never'}],
+
     '@typescript-eslint/no-unused-vars': [
       'error',
       { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
