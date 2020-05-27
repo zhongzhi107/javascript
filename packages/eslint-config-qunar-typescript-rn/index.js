8e1f985e195d2f6056a4b09e53c148d8b64fc589
zhi.zhong <zhong.zhi@163.com>
2020-05-27 17:41:28 +0800

    feat: add @qnpm/eslint-plugin-event-listener

--- packages/eslint-config-qunar-typescript-rn/index.js
+++ packages/eslint-config-qunar-typescript-rn/index.js
@@ -1,6 +1,9 @@
 const rules = require('eslint-config-qunar-rn/rules');
 
 module.exports = {
-  extends: ['eslint-config-qunar-typescript-react'],
+  extends: [
+    'eslint-config-qunar-typescript-react',
+    'plugin:@qnpm/eslint-plugin-event-listener/recommended'
+  ],
   rules
 };
--- packages/portal-eslint-setup/eslint-config-qunar-react.js
+++ packages/portal-eslint-setup/eslint-config-qunar-react.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ['eslint-config-qunar-react/server']
+  extends: [
+    'eslint-config-qunar-react/server',
+    'plugin:@qnpm/eslint-plugin-event-listener/recommended'
+  ],
+  plugins: [
+    'event-listener'
+  ]
 };
